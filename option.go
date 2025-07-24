be9a8b5a939f5205f399b3717fa70a2049b8bd1f
Antoni Szymański <antoni.szymanski+github@proton.me>
2025-07-24 21:08:21 +0200

    docs: add doc comments to Option methods

--- option.go
+++ option.go
@@ -8,22 +8,27 @@ type Option[T any] struct {
 	valid bool
 }
 
+// Return a `Some` value containing the given value.
 func Some[T any](value T) Option[T] {
 	return Option[T]{value: value, valid: true}
 }
 
+// Returns a `None` value of type T.
 func None[T any]() Option[T] {
 	return Option[T]{}
 }
 
+// Returns true if the option is a `Some` value.
 func (o Option[T]) IsSome() bool {
 	return o.valid
 }
 
+// Returns true if the option is a `None` value.
 func (o Option[T]) IsNone() bool {
 	return !o.valid
 }
 
+// Returns the contained `Some` value or panics with a custom panic message provided by msg.
 func (o Option[T]) Expect(msg string) T {
 	if o.valid {
 		return o.value
@@ -32,6 +37,7 @@ func (o Option[T]) Expect(msg string) T {
 	}
 }
 
+// Returns the contained `Some` value or panics with a generic message.
 func (o Option[T]) Unwrap() T {
 	if o.valid {
 		return o.value
@@ -40,6 +46,7 @@ func (o Option[T]) Unwrap() T {
 	}
 }
 
+// Returns the contained `Some` value or a provided fallback.
 func (o Option[T]) UnwrapOr(fallback T) T {
 	if o.valid {
 		return o.value
@@ -48,6 +55,7 @@ func (o Option[T]) UnwrapOr(fallback T) T {
 	}
 }
 
+// Returns the contained `Some` value or computes it from a closure.
 func (o Option[T]) UnwrapOrElse(fn func() T) T {
 	if o.valid {
 		return o.value
@@ -56,6 +64,7 @@ func (o Option[T]) UnwrapOrElse(fn func() T) T {
 	}
 }
 
+// Returns the contained `Some` value or the zero value for type T.
 func (o Option[T]) UnwrapOrZero() T {
 	return o.value
 }
